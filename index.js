778394c9a5db59294318ba3ef663d955dc915abb
Vishal Reddy <vishal.reddy@liferay.com>
2016-10-18 09:37:16 -0700

    config

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var morgan = require('morgan');
 var path = require('path');
 var app = express();
 var bodyParser = require('body-parser')
+var wedeploy = require('wedeploy')
 
 
 app.use( bodyParser.json() );       // to support JSON-encoded bodies
@@ -17,6 +18,13 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname + '/public/index.html'));
 });
+
+// POST /jenkins gets urlencoded bodies
+app.post('/jenkins', bodyParser, function (req, res) {
+    if (!req.body) return res.sendStatus(400)
+    console.log(body)
+    res.send("done")
+})
 
 app.listen(80, function () {
   console.log('Listening on port 80');
