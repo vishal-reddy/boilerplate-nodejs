8921486b362014b54574f2f9f73b444bc7c3cff8
Vishal Reddy <vishal.reddy@liferay.com>
2016-10-17 17:55:23 -0700

    config-test

--- index.js
+++ index.js
@@ -2,11 +2,20 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var app = express();
+var bodyParser = require('body-parser')
+
+
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+    extended: true
+})); 
 
 app.use(morgan('combined'));
 
+
 app.get('/', function (req, res) {
-	res.sendFile(path.join(__dirname + '/public/index.html'));
+    res.sendFile(path.join(__dirname + '/public/index.html'));
 });
 
 app.listen(80, function () {
